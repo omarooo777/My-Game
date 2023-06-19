7ed0f015a133711e1ef7bc5bc227270bd103dc0e
omar khaled <omarkhaliid77@gmail.com>
2023-06-20 01:35:57 +0200

    add quiz app game files and styling

---
+++ JavaScript/quiz app.js
@@ -0,0 +1,17 @@
+// let details = document.querySelector("details");
+
+// document.body.onclick = function() {
+//   details.removeAttribute("open")
+// }
+
+// let myRequest = new XMLHttpRequest();
+
+// myRequest.onreadystatechange = function() {
+//   if(this.readyState == 4 && this.status == 200) {
+//     let jsObject = JSON.parse(this.responseText);
+//     console.log(jsObject.length)
+//   }
+// }
+
+// myRequest.open("GET", "../JSON/html_questions.json", true);
+// myRequest.send();
